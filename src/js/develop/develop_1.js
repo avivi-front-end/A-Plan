65494e2278ff0a30100cc3f24219da1d7e19dc46
trolizmaslom <trolizmaslom@gmail.com>
2017-03-09 17:12:42 +0200

    start

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -1,14 +1,20 @@
-
+function slickInit(){
+    var slick =$('.slick-init');
+    if(slick.length>0){
+        slick.slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+        slick.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+            var img = $('.slick-active').data('img');
+            if (img!=undefined){$('.slider').attr('style','background-image: url('+img+');');}
+        });
+    }
+}
 
 
 $(document).ready(function(){
-
+    slickInit();
 });
 
-$(window).load(function(){
-
-});
-
-$(window).resize(function(){
-
-});

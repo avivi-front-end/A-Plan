73266f70b5844a4cdd844d32f08e181d3342215a
trolizmaslom <trolizmaslom@gmail.com>
2017-03-13 17:17:52 +0200

    monday stuck

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -22,9 +22,61 @@ function radioLogic(){
 }
 function getKoloText() {
     if($('.kolo').length>0){
+        $('.scripted-text').removeClass('show');
         var html = $('.kolo li.active').find('.kolo__txt').html();
-        $('.scripted-text').html(html);
+        setTimeout(function () {
+            $('.scripted-text').html(html);
+            $('.scripted-text').addClass('show');
+        },200);
+
     }
+
+}
+function koloSlider(){
+    var slider = $('.koleso__slider');
+    if(slider.length>0){
+        var all = slider.find('li');
+        $('.koleso__wraper .slick-prev').click(function () {
+            slidePrev(slider);
+        });
+        $('.koleso__wraper .slick-next').click(function () {
+            slideNext(slider);
+        });
+        all.click(function(){
+            all.removeClass('active');
+            $(this).addClass('active');
+            reinitSlider(slider);
+        })
+    }
+}
+function slidePrev(slider){
+   var act = slider.find('li.active');
+   if(act.prev().length>0){
+       act.removeClass('active');
+       act.prev().addClass('active');
+       reinitSlider(slider);
+   }
+}
+function slideNext(slider){
+    var act = slider.find('li.active');
+    if(act.next().length>0){
+        act.removeClass('active');
+        act.next().addClass('active');
+        reinitSlider(slider);
+    }
+}
+function reinitSlider(slider){
+    var all = slider.find('li');
+    all.removeClass('left0 left1 left2 left3 left4 left5 left6 left7 left8 left9 left10 left11 left12');
+    var act = slider.find('li.active');
+    var ind = act.index();
+    all.each(function(index){
+        var equal = 6-ind+index;
+        $(this).addClass('left'+equal);
+    });
+    getKoloText();
+
+
 
 }
 
@@ -32,5 +84,6 @@ $(document).ready(function(){
     slickInit();
     radioLogic();
     getKoloText();
+    koloSlider();
 });
 
